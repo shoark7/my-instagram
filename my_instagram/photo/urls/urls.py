3acfc41cf2f4da6d0eae08cab535d38f4b845aff
Sunghwan Park <shoark7@gmail.com>
2016-11-14 15:44:54 +0900

    Make photo add CBV

--- my_instagram/photo/urls/urls.py
+++ my_instagram/photo/urls/urls.py
@@ -3,5 +3,7 @@ from photo import views
 
 
 urlpatterns = [
-    url(r'list', views.photo_list, name='photo_list'),
+    # url(r'list', views.photo_list, name='photo_list'),
+    url(r'^list/$', views.PhotoList.as_view(), name='photo_list'),
+    url(r'^add/$', views.PhotoAdd.as_view(), name='photo_add'),
 ]
--- my_instagram/photo/views.py
+++ my_instagram/photo/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
 from .models import Photo
+# from django.views.generic import ListView
 # Create your views here.
 
-def photo_list(request):
-    photos = Photo.objects.all()
-    context = {
-        'photos': photos,
-    }
-    return render(request, 'photo/photo_list.html', context)
+# def photo_list(request):
+#     photos = Photo.objects.all()
+#     context = {
+#         'photos': photos,
+#     }
+#     return render(request, 'photo/photo_list.html', context)
+
+
+class PhotoList(ListView):
+    model = Photo
+    paginate_by = 5
+    context_object_name = 'photos'
+    # queryset = Photo.objects.filter('-created_date')
+
+
+class PhotoAdd(CreateView):
+    model = Photo
+    fields = ['photo', 'author', 'content',]
+    template_name = 'photo/photo_add.html'
+    success_url = '/photo/list'
+
+    # def form_valid(self, form):
